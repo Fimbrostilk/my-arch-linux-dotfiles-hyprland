e3590c1c3faa5224accc1fa4a527be05442dec06
Fimbrostilk <angelpadron407@gmail.com>
2025-12-12 22:11:20 -0600

    Configuraciones varias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-export PATH="$HOME/Ndless/ndless-sdk/toolchain/install/bin:$HOME/Ndless/ndless-sdk/bin:$PATH"
 
 # overwrite greeting
 # potentially disabling fastfetch
